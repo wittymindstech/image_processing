c3595a198f77bc6116fab65138ea9276b3abad58
wiTTyMindsTech <63321726+wittymindstech@users.noreply.github.com>
2021-06-24 23:56:18 +0530

    Create add_watermark.py

---
+++ add_watermark.py
@@ -0,0 +1,27 @@
+#Import required Image library
+from PIL import Image, ImageDraw, ImageFont
+
+#Create an Image Object from an Image
+im = Image.open('images/boy.jpg')
+width, height = im.size
+
+draw = ImageDraw.Draw(im)
+text = "sample watermark"
+
+font = ImageFont.truetype('arial.ttf', 36)
+textwidth, textheight = draw.textsize(text, font)
+
+# calculate the x,y coordinates of the text
+margin = 10
+x = width - textwidth - margin
+y = height - textheight - margin
+
+# draw watermark in the bottom right corner
+draw.text((x, y), text, font=font)
+im.show()
+
+#Save watermarked image
+
+rgb_im = im.convert('RGB')
+rgb_im.save('audacious.jpg')
+#im.save('images/watermark.jpg')
